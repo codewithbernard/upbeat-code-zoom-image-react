85de25a00ab422b182a9d59d03792b2381b0acfe
Bernard <bado.bernard94@gmail.com>
2022-04-12 12:29:16 +0100

    scaling image down

--- src/components/ZoomImage/ZoomImage.jsx
+++ src/components/ZoomImage/ZoomImage.jsx
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { useRef, useMemo, useEffect } from "react";
 
-const ZoomImage = () => {
-  return <div>ZoomImage</div>;
+const ZoomImage = ({ image }) => {
+  const canvasRef = useRef(null);
+  const containerRef = useRef(null);
+  const observer = useRef(null);
+  const background = useMemo(() => new Image(), [image]);
+
+  useEffect(() => {
+    observer.current = new ResizeObserver((entries) => {
+      entries.forEach(({ target }) => {
+        const { width, height } = background;
+        // If width of the container is smaller than image, scale image down
+        if (target.clientWidth < width) {
+          // Calculate scale
+          const scale = target.clientWidth / width;
+
+          // Redraw image
+          canvasRef.current.width = width * scale;
+          canvasRef.current.height = height * scale;
+          canvasRef.current
+            .getContext("2d")
+            .drawImage(background, 0, 0, width * scale, height * scale);
+        }
+      });
+    });
+    observer.current.observe(containerRef.current);
+
+    return () => observer.current.unobserve(containerRef.current);
+  }, []);
+
+  useEffect(() => {
+    background.src = image;
+
+    if (canvasRef.current) {
+      background.onload = () => {
+        // Get the image dimensions
+        const { width, height } = background;
+        canvasRef.current.width = width;
+        canvasRef.current.height = height;
+
+        // Set image as background
+        canvasRef.current.getContext("2d").drawImage(background, 0, 0);
+      };
+    }
+  }, [background]);
+
+  return (
+    <div ref={containerRef}>
+      <canvas ref={canvasRef} />
+    </div>
+  );
 };
 
 export default ZoomImage;
